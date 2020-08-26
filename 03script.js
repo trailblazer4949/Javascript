c42724ada08159a7174644d65b77e53fbe298529
Coder_49 <47142802+trailblazer4949@users.noreply.github.com>
2020-08-26 16:25:23 +0530

    Create 03script.js

---
+++ 03script.js
@@ -0,0 +1,20 @@
+//Conditions 
+/*
+Author - Bharat Vyas
+Learn about :
+    If-else 
+*/
+temperature = 30;
+if (temperature < 30) {
+    console.log("Temperature is cold");
+}
+if (temperature > 40) {
+    console.log("It's hot!");
+}
+if (temperature == 30) {
+    console.log("It's normal");
+}
+else {
+    console.log("You will never reach here (:-p)");
+}
+//Output : It's normal
